668058259e2e84b749ac03f781360c789001e7e2
HNOONa-0 <59091536+HNOONa-0@users.noreply.github.com>
2021-04-12 15:24:39 +0200

    Create Two numbers.cpp

---
+++ CodeForces/Two numbers.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << "floor " << x << " / " << y << " = " << floor(static_cast<double>(x) / y) << endl;
+    cout << "ceil " << x << " / " << y << " = " << ceil(static_cast<double>(x) / y) << endl;
+    cout << "round " << x << " / " << y << " = " << round(static_cast<double>(x) / y) << endl;
+    return 0;
+}
